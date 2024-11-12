097d3701a5a5f25ed58313439f84762b72292d0d
dengtesla <dengtesla@163.com>
2024-11-13 01:14:26 +0800

    调整 cmakefile 的结构

--- compile.sh
+++ compile.sh
@@ -2,11 +2,15 @@ PROTOC="/mnt/public/szdeng/protobuf_src/protobuf/protoc"
 
 rm -rf proto_out
 mkdir proto_out
+cp proto/CMakeLists.txt proto_out/
 
 cd proto
 protos=`find . -name "*.proto"`
 ${PROTOC} ${protos} --cpp_out=../proto_out/
 
+if [[ ! -d ../build ]];then
+  mkdir ../build
+fi
 cd ../build
 cmake ..
 make
